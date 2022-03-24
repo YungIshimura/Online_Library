af6d353e53df31f30e32b1f7759ae2af834bcb96
Mark Tereshchenko <83189636+YungIshimura@users.noreply.github.com>
2022-03-24 04:51:34 -0400

    Fix livereload cli and naming

--- render_website.py
+++ render_website.py
@@ -8,15 +8,15 @@ from more_itertools import chunked
 
 def main():
     template = get_template()
-    render_page(template)
-    rebuilt()
+    render_pages(template)
+    rebuild()
     server = Server()
-    server.watch("template.html", main)
-    server.watch("index.html", rebuilt)
-    server.serve(root='.')
+    server.watch('template.html', main)
+    server.watch('pages/*.html', rebuild)
+    server.serve(root='pages/')
 
 
-def rebuilt():
+def rebuild():
     print("server rebuilt")
 
 
@@ -30,17 +30,17 @@ def get_template():
     return template
 
 
-def render_page(template, directory="pages/", number_pages=10):
+def render_pages(template, directory="pages/", number_pages=10):
     os.makedirs(directory, exist_ok=True)
     with open('books.json', 'r') as file:
-        books_json = json.load(file)
+        books = json.load(file)
 
-    books_chunked = list(chunked(chunked(books_json, 2), number_pages))
+    books_chunked = list(chunked(chunked(books, 2), number_pages))
     file_names = [f"index{page+1}.html" for page in range(len(books_chunked))]
 
     for number, books in enumerate(books_chunked, start=1):
         rendered_page = template.render(books=books, pages=file_names,
-                                        page_number=number,     
+                                        page_number=number,
                                         number_pages=len(file_names))
         path = os.path.join(directory, f"index{number}.html")
         with open(path, "w") as file:
